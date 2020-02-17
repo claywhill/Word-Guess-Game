b4134132d2b63523ee4b0bed5872a5dffe7a368a
Clayton Hill <clayman0123@yahoo.com>
2020-02-16 19:10:40 -0700

    cleaned up code

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,18 +1,40 @@
-// creates an array of letters to be guessed
-var letters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+// I need to display the words as blanks
+// I need the user to guess a letter
+// If user guesses wrong, place that letter in "letters guessed"
+// If user guesses correctly, reveal letter in the blank
+// If user guesses entire word, increase "wins"
 
-// creates variable to hold number of wins starting at 0
+
+
+// words available for guessing
+var words = ["yosemite", "glacier", "yellowstone", "zion", "rocky mountain", "olympic", "grand canyon", "great sand dunes", "hot springs", "great smoky mountains", "arches", "canyonlands", "denali", "joshua tree"];
+
+// causes words to be selected at random
+var word = words[Math.floor(Math.random() * words.length)];
+
+// number of guesses remaining
+var numOfGuesses = 10;
+
+// variable to hold number of wins starting at 0
 var wins = 0;
 
-// creates varibales that hold references to places in the HTML where we want to display things
-var instructionsText = document.getElementById("instructions-text")
-var winsText = document.getElementById("wins-text")
-var currentWord = document.getElementById("currentword-text")
-var userGuess = document.getElementById("userguess-text")
-var guessRemaining = document.getElementById("guessremaining")
-var lettersGuessed = document.getElementById("lettersguessed")
+// variable to store the current word to be guessed
+var wordToBeGuessed = [];
 
-// looping throgh each item in the array and logging a message to the console
-for (var i = 0; i < letters.length; i++) {
-console.log("I guessed " + letters[i]);
+// loops through the list of random words and gives each letter in that word a "_"
+ for (var i = 0; i < word.length; i++) {
+    wordToBeGuessed[i] = "_";
+    }
+
+// variable that holds the number of letters remaining to be guessed in the current word
+var remainingLetters = word.length;
+
+// ****** GAME LOOP ******
+
+while (remainingLetters > 0) {
+    document.getElementById("blanks").innerHTML = wordToBeGuessed.join(" ");
+
+    var guess = onkeydown();
 }
+
+
